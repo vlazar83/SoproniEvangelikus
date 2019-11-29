c05cb0e63e4a0624eedd0837d41043ecd24e2574
Lázár Viktor <NLYVIK@wstw.energy-it.net>
2019-11-29 18:02:33 +0100

    First steps with added Map in EventDetailsActivity

--- app/src/main/java/com/vlazar83/sopronievangelikus/EventDetailsActivity.java
+++ app/src/main/java/com/vlazar83/sopronievangelikus/EventDetailsActivity.java
@@ -7,9 +7,19 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EventDetailsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class EventDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private Event eventObject;
+    private GoogleMap mMap;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +36,22 @@ public class EventDetailsActivity extends AppCompatActivity {
 
         TextView fullNameTextView = findViewById(R.id.event_full_name);
         fullNameTextView.setText(eventObject.getFullName());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // Add a marker in Sopron and move the camera
+        LatLng sydney = new LatLng(47.685276, 16.589422);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sopron"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 }
